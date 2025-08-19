bfdaa600831e69c1833e575ba6557ee6c41e695c
yareach8345 <mine8345@naver.com>
2025-08-19 18:03:01 +0900

    test - get_message_type_from_topic에 대한 테스트 작성

--- test/test_logic/mqtt/parsing/test_topic_parsing.cpp
+++ test/test_logic/mqtt/parsing/test_topic_parsing.cpp
@@ -13,13 +13,34 @@ const String channel_state_changed_topic = "chzzk-notification/channel/chzzk/c0d
 const String channel_image_changed_topic = "chzzk-notification/channel/chzzk/ca0551b82e3c45a15012145211bacd21/image";
 
 void test_parse_refresh_message() {
-    MessageType result = get_message_type_from_topic(refreshed_topic);
-
+    const MessageType result = get_message_type_from_topic(refreshed_topic);
     TEST_ASSERT_EQUAL(REFRESHED, result);
 }
 
-void test_topic_parsing() {
-    // todo: topic_parsing 테스트 작성
+void test_parse_updated_message() {
+    const MessageType result = get_message_type_from_topic(updated_topic);
+    TEST_ASSERT_EQUAL(UPDATED, result);
+}
 
+void test_parse_channel_info_changed_message() {
+    const MessageType result = get_message_type_from_topic(channel_info_changed_topic);
+    TEST_ASSERT_EQUAL(INFO_CHANGED, result);
+}
+
+void test_parse_channel_state_changed_message() {
+    const MessageType result = get_message_type_from_topic(channel_state_changed_topic);
+    TEST_ASSERT_EQUAL(STATE_CHANGED, result);
+}
+
+void test_parse_channel_image_changed_message() {
+    const MessageType result = get_message_type_from_topic(channel_image_changed_topic);
+    TEST_ASSERT_EQUAL(IMAGE_CHANGED, result);
+}
+
+void test_topic_parsing() {
     RUN_TEST(test_parse_refresh_message);
+    RUN_TEST(test_parse_updated_message);
+    RUN_TEST(test_parse_channel_info_changed_message);
+    RUN_TEST(test_parse_channel_state_changed_message);
+    RUN_TEST(test_parse_channel_image_changed_message);
 }
